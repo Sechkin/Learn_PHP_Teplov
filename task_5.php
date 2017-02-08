2a8d3ab5b16f462e9f7ba319c82825336b336265
Vladimir <sechkin.vlad@gmail.com>
2017-02-08 16:44:39 +0200

    task5 finished second time

--- task_5.php
+++ task_5.php
@@ -13,14 +13,15 @@
     <body>
         <?php
         $array = $array1 = $array2 = array(1, 2, 3, 4, 5, 6, 7, 8, 9);
-        $x = $y = 1;
+        $x = $y = 0;
         print_r($array);
         echo "<br>";
         while ($x < count($array1)) {
-            if ($x % 2 == 0) {
+            
+            if (($x+1) % 2 == 0) {
                 $array1[$x] += 2;
             }
-            if ($x % 3 == 0) {
+            if (($x+1) % 3 == 0) {
                 $array1[$x] += 3;
             }
             $x++;
@@ -28,19 +29,19 @@
         print_r($array1);
         echo "<br>";
         do {
-            if ($y % 2 == 0) {
+            if (($y+1) % 2 == 0) {
                 $array2[$y] += 2;
-            } elseif ($y % 3 == 0) {
+            } elseif (($y+1) % 3 == 0) {
                 $array2[$y] += 3;
             }
         } while (++$y < count($array2));
         print_r($array2);
         echo "<br>";
-        for ($x = 1; $x < count($array); $x++) {
+        for ($x = 0; $x < count($array); $x++) {
             switch ($x) {
-                case ($x % 2 == 0) : $array[$x] += 2;
+                case (($x+1) % 2 == 0) : $array[$x] += 2;
                     break;
-                case ($x % 3 == 0) : $array[$x] += 3;
+                case (($x+1) % 3 == 0) : $array[$x] += 3;
                     break;
             }
         }
